b8277c8be99bbd53e90edd27ebc0bfa92d5bfa4e
User <user@example.com>
2025-12-05 18:17:35 +0900

    Add custom Channel.io button to floating menu

--- components/ChannelTalk.tsx
+++ components/ChannelTalk.tsx
@@ -12,7 +12,8 @@ export default function ChannelTalk() {
           (function(){var w=window;if(w.ChannelIO){return w.console.error("ChannelIO script included twice.");}var ch=function(){ch.c(arguments);};ch.q=[];ch.c=function(args){ch.q.push(args);};w.ChannelIO=ch;function l(){if(w.ChannelIOInitialized){return;}w.ChannelIOInitialized=true;var s=document.createElement("script");s.type="text/javascript";s.async=true;s.src="https://cdn.channel.io/plugin/ch-plugin-web.js";var x=document.getElementsByTagName("script")[0];if(x.parentNode){x.parentNode.insertBefore(s,x);}}if(document.readyState==="complete"){l();}else{w.addEventListener("DOMContentLoaded",l);w.addEventListener("load",l);}})();
 
           ChannelIO('boot', {
-            "pluginKey": "bf07e569-5645-4ed0-a351-bd7d3c0d5d7b"
+            "pluginKey": "bf07e569-5645-4ed0-a351-bd7d3c0d5d7b",
+            "hideChannelButtonOnBoot": true
           });
         `,
       }}
