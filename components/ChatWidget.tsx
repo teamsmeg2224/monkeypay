c95aa1995f817efc09b7072532b3c44b1c192260
User <teamsmeg0619@gmail.com>
2025-12-31 00:50:29 +0900

    Simplify ChatWidget injection to fix ReferenceError

--- components/ChatWidget.tsx
+++ components/ChatWidget.tsx
@@ -1,33 +1,27 @@
 "use client";
 
-import { useEffect } from "react";
 import Script from "next/script";
 
 export default function ChatWidget() {
-    useEffect(() => {
-        // Configure chat widget
-        (window as any).chatWidgetConfig = {
-            launcher: {
-                hidden: true, // We will use our own floating button to trigger it
-            }
-        };
-    }, []);
-
     return (
         <>
             <Script
-                id="chat-widget-script"
+                src="https://chat-widget-alpha-three.vercel.app/embed.js"
                 strategy="afterInteractive"
+                data-site-id="3a6912b3-1322-4d90-b3bb-c41c568566bb"
+                onLoad={() => {
+                    console.log("Chat widget script loaded");
+                }}
+            />
+            <script
                 dangerouslySetInnerHTML={{
                     __html: `
-            (function(w, d, s, o, f, js, fjs) {
-              w[o] = w[o] || function() { (w[o].q = w[o].q || []).push(arguments) };
-              js = d.createElement(s), fjs = d.getElementsByTagName(s)[0];
-              js.id = o; js.src = f; js.async = 1;
-              js.setAttribute('data-site-id', '3a6912b3-1322-4d90-b3bb-c41c568566bb');
-              fjs.parentNode.insertBefore(js, fjs);
-            }(window, document, 'script', 'ChatWidget', 'https://chat-widget-alpha-three.vercel.app//embed.js'));
-          `,
+            window.chatWidgetConfig = {
+              launcher: {
+                hidden: true
+              }
+            };
+          `
                 }}
             />
         </>
