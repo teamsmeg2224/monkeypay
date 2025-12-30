e2f6b136e052efbbb82950b005b3e56d1ff0a5eb
User <teamsmeg0619@gmail.com>
2025-12-31 00:27:49 +0900

    Fix manifest type error

--- app/manifest.ts
+++ app/manifest.ts
@@ -16,7 +16,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/favicon.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "any",
       },
       {
         src: "/favicon.ico",
